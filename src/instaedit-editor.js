a80455336c9c842c15b92dd74133e9cfdf6e7ae0
Jan Palounek <jan@palounek.com>
2012-05-30 12:09:06 +0200

    Editor content serving.

--- src/instaedit-editor.js
+++ src/instaedit-editor.js
@@ -1,1 +1,10 @@
-window.opener.edited_content = 'new verision of the file';
+window.onload = function() {
+	var site_content = window.opener.site_content;
+
+	var editor_content = document.getElementById('editor');
+	editor_content.innerHTML = site_content.replace(/^\s+|\s+$/g,"");
+
+	var editor = ace.edit("editor");
+
+	window.opener.edited_content = 'New verision of the file submitted from editor.';
+}
--- src/instaedit-worker.js
+++ src/instaedit-worker.js
@@ -4,6 +4,4 @@ var editor_content = 'This will be displayed in editor.';
 editor = window.open('../src/editor.html', 'Instaedit editor');
 editor.focus();
 
-console.log(site_content)
-
 // Changes receiving here
