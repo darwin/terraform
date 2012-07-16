b804eb55f94058c0dfdc3a50a2b38f0b4bcdd96f
Jan Palounek <jan@palounek.com>
2012-07-16 22:56:21 +0200

    Commit api bugfixed and extended.

--- src/github.js
+++ src/github.js
@@ -80,35 +80,29 @@ GithubAuth.prototype.sendCommitRequest = function (data, code, url, cb) {
   reqDataRaw.token = code;
   reqDataRaw.target = url;
 
-  var reqData = JSON.stringify(reqDataRaw);
-/*
-  var request = new XMLHttpRequest();
-  request.open('POST', this.getAuthServerURL() + '/commit', true);
-  request.setRequestHeader("Content-Type", "application/json");
-  
-  request.send(reqData);
-
-*/
-
-/*
-  $.ajax({
-    type: 'POST',
-    url: this.getAuthServerURL() + '/commit',
-    data: reqData,
-    success: cb('success'),
-    dataType: cb('failed')
-  });
-*/
-
   console.log('Sending data to ' + this.getAuthServerURL() + '/commit');
-  $.post(this.getAuthServerURL() + '/commit', reqData,
-    function(data) {
-      console.log('API retrieved ');
-      console.log(data);
-      cb('failed'); // Just for debugging of course
-    }, 
-    "json"
-  );
+  
+  var self = this;
+  var url = 'https://api.github.com/repos/' + url.split('/')[3] + '/' + url.split('/')[4] + '/git/refs/heads/' + url.split('/')[5];
+  jQuery.getJSON(url + "?callback=?", {}, function(response) {
+    reqDataRaw.tree = response.data.object.sha;
+    var reqData = JSON.stringify(reqDataRaw);
+
+    var request = new XMLHttpRequest();
+    request.open('POST', self.getAuthServerURL() + '/commit', true);
+    request.setRequestHeader("Content-Type", "application/json");
+  
+    request.send(reqData);
+
+    request.onloadend = function () {
+      console.log(request.statusCode);
+      if(request.status == 500) {
+        cb('failed');
+      } else {
+        cb('success');
+      }
+    }
+  });
 }
 
 GithubAuth.prototype.setGithubToken = function (token) {
